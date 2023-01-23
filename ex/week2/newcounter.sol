a9c2a7eaab315f48084a72e3b70be8889829febd
tawfiq2 <62226647+tawfiq2@users.noreply.github.com>
2023-01-23 12:02:24 +0300

    Create newcounter.sol

---
+++ ex/week2/newcounter.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.17;
+contract HelloWorld {
+ //address owner=
+ //bool any== fa
+ //string greeting = "Hello World";
+ //Constructor function
+
+ uint public counter;
+ function hi() public returns(uint) {
+unit newcounter=counter;
+     return newcounter;
+ }
+
+
+ }
+
+
+      //public view
+     //public pure
