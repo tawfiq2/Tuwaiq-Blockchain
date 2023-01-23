bb3a305a1ae916ffca4624a32c8d7cb413d4e032
tawfiq2 <62226647+tawfiq2@users.noreply.github.com>
2023-01-23 11:59:46 +0300

    Create HelloWorld.sol

---
+++ ex/week2/HelloWorld.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.15;
+contract HelloWorld {
+ address owner=
+ bool any== fa
+ string greeting = "Hello World";
+ // Constructor function
+ function HelloWorld () public {
+ owner = msg.sender;
+ }
